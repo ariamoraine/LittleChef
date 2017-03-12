7c526f5965109dc0e61dc7a2c692f5743f4f2700
Aria Moraine <ariamoraine@gmail.com>
2017-03-12 18:15:37 -0400

    fixed sorting issue

--- app/containers/AppContainer.jsx
+++ app/containers/AppContainer.jsx
@@ -6,9 +6,9 @@ import { receiveRecipes } from '../reducers/recipe'
 function sortRecipes (recipeArray) {
   const sortedRecipes = recipeArray.sort(function(a, b) {
     return parseFloat(a.updated_at) - parseFloat(b.updated_at)
-  }).reverse()
+  })
   sortedRecipes[0]['featured'] = true
-
+  console.log("SORTED?", sortedRecipes)
   return sortedRecipes
 }
 
