644df3d61d8e5dfb6c79e4dc75f6b21fa1409802
Aria Moraine <ariamoraine@gmail.com>
2017-03-12 22:46:57 -0400

    working on adding in form for addRecipe

--- app/components/AddRecipe.jsx
+++ app/components/AddRecipe.jsx
@@ -1,7 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
+import Paper from 'material-ui/paper'
+import TextField from 'material-ui/TextField'
 
-export default function (props) {
-  return (
-    <h1>HIZZZZZZ</h1>
-  )
+export default class AddRecipe extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      recipeName: '',
+      recipePhoto: '',
+      recipeIngredients: [],
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  // handleChange = (event) => {
+  //   console.log(event.target.value)
+  //   this.setState({
+  //     value: event.target.value,
+  //   });
+  // };
+
+  handleChange(input, event) {
+    console.log(input)
+    const change = {};
+    const value = event.target.value
+    change[input] = value
+
+    this.setState(change)
+  }
+
+  render() {
+    return (
+      <Paper>
+      <div>
+        <TextField
+          value="recipeName"
+          hintText="Recipe Name"
+          onChange={this.handleChange}
+        />
+        <TextField
+          value="recipePhoto"
+          hintText="Recipe Name"
+          onChange={this.handleChange}
+        />
+      </div>
+      </Paper>
+    );
+  }
 }
